c5f83ef52e9ea0cd641f4f3de75b4eb4316c30f6
unknown <wesley@TI08.unimedjpr.local>
2015-05-06 16:09:08 -0400

    projeto fase 3

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -25,15 +25,19 @@ Route::get('user/{id?}', function($id = 123){
 */
 
 Route::group(['prefix'=>'admin'], function(){
-    Route::get('categories', 'AdminCategoriesController@index');
-    Route::post('categories', 'AdminCategoriesController@store');
-    Route::get('categories', 'AdminCategoriesController@update');
-    Route::get('categories', 'AdminCategoriesController@delete');
+    Route::get('categories',['as'=>'categories','uses'=>'AdminCategoriesController@index']);
+    Route::post('categories',['as'=>'categories.store','uses'=>'AdminCategoriesController@store']);
+    Route::get('categories/create',['as'=>'categories.create','uses'=>'AdminCategoriesController@create']);
+    Route::get('categories/{id}/destroy',['as'=>'categories.destroy','uses'=>'AdminCategoriesController@destroy']);
+    Route::get('categories/{id}/edit',['as'=>'categories.edit','uses'=>'AdminCategoriesController@edit']);
+    Route::put('categories/{id}/update',['as'=>'categories.update','uses'=>'AdminCategoriesController@update']);
 
-    Route::get('products', 'AdminProductsController@index');
-    Route::post('products', 'AdminProductsController@store');
-    Route::get('products', 'AdminProductsController@update');
-    Route::get('products', 'AdminProductsController@delete');
+    Route::get('products',['as'=>'products','uses'=>'AdminProductsController@index']);
+    Route::post('products',['as'=>'products.store','uses'=>'AdminProductsController@store']);
+    Route::get('products/create',['as'=>'products.create','uses'=>'AdminProductsController@create']);
+    Route::get('products/{id}/destroy',['as'=>'products.destroy','uses'=>'AdminProductsController@destroy']);
+    Route::get('products/{id}/edit',['as'=>'products.edit','uses'=>'AdminProductsController@edit']);
+    Route::put('products/{id}/update',['as'=>'products.update','uses'=>'AdminProductsController@update']);
 });
 
 /*
